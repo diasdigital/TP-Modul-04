71a42cda49decdea7b821e89d32bf0fe94aa35ef
diasdigital <78405123+diasdigital@users.noreply.github.com>
2024-03-13 01:35:40 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_1302223134/DoorMachine.cs
@@ -0,0 +1,58 @@
+﻿namespace tpmodul4_1302223134;
+public enum DoorState
+{
+    Terkunci, Terbuka
+}
+public enum Trigger
+{
+    KunciPintu, BukaPintu
+}
+
+internal class DoorMachine
+{
+    public class Transition
+    {
+        public DoorState stateAwal;
+        public DoorState stateAkhir;
+        public Trigger trigger;
+
+        public Transition(DoorState stateAwal, DoorState stateAkhir, Trigger trigger)
+        {
+            this.stateAwal = stateAwal;
+            this.stateAkhir = stateAkhir;
+            this.trigger = trigger;
+        }
+    }
+
+    Transition[] transisi =
+    {
+        new Transition(DoorState.Terkunci, DoorState.Terbuka, Trigger.BukaPintu),
+        new Transition(DoorState.Terkunci, DoorState.Terkunci, Trigger.KunciPintu),
+        new Transition(DoorState.Terbuka, DoorState.Terkunci, Trigger.KunciPintu),
+        new Transition(DoorState.Terbuka, DoorState.Terbuka, Trigger.BukaPintu)
+    };
+
+    public DoorState currentState = DoorState.Terkunci;
+
+    public DoorState getNextState(DoorState stateAwal, Trigger trigger)
+    {
+        DoorState stateAkhir = stateAwal;
+        foreach (var t in transisi)
+        {
+            Transition perubahan = t;
+            if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+            {
+                stateAkhir = perubahan.stateAkhir;
+            }
+        }
+        return stateAkhir;
+    }
+
+    public void ActivateTrigger(Trigger trigger)
+    {
+        currentState = getNextState(currentState, trigger);
+
+        Console.WriteLine(currentState == DoorState.Terkunci ? "Pintu terkunci" : "Pintu terbuka");
+    }
+
+}
--- tpmodul4_1302223134/Program.cs
+++ tpmodul4_1302223134/Program.cs
@@ -2,3 +2,8 @@
 
 KodePos objKodePos = new KodePos();
 Console.WriteLine("Kode pos Batununggal adalah " + objKodePos.getKodePos(KodePos.enumKelurahan.Batununggal));
+
+DoorMachine objDoorMachine = new DoorMachine();
+Console.WriteLine("\nKeadaan pintu: " + objDoorMachine.currentState);
+Console.WriteLine("Menggunakan trigger BukaPintu");
+objDoorMachine.ActivateTrigger(Trigger.BukaPintu);
